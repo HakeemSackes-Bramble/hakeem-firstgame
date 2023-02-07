a31487f44e96da6f9a9de3647484356aa35076b2
Hakeem Sackes-Bramble <hakksaxx@outlook.com>
2023-02-07 17:28:27 +0000

    setting up main game loop in order to allow player to move around

--- main.py
+++ main.py
@@ -1,7 +1,24 @@
 import pygame
 pygame.init()
-win = pygame.display.set_mode((1000, 1000))
+win = pygame.display.set_mode((1000, 500))
 color = (255, 255, 255)
 win.fill(color)
 pygame.display.flip()
 pygame.display.set_caption("First Game")
+
+#initial values needed for motion
+x = 50
+y = 50
+width = 40
+height = 60
+vel = 5
+run = True
+
+while run:
+    pygame.time.delay(100) # This will delay the game the given amount of milliseconds. In our casee 0.1 seconds will be the delay
+
+    for event in pygame.event.get():  # This will loop through a list of any keyboard or mouse events.
+        if event.type == pygame.QUIT: # Checks if the red button in the corner of the window is clicked
+            run = False  # Ends the game loop
+
+pygame.quit()  
