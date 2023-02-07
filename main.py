b318e7079c5b9d565d1bc69e151f53fb66ab445d
Hakeem Sackes-Bramble <hakksaxx@outlook.com>
2023-02-07 15:44:43 +0000

    color of the window

--- main.py
+++ main.py
@@ -1,3 +1,7 @@
 import pygame
 pygame.init()
-win = pygame.display.set_mode((500, 500))
+win = pygame.display.set_mode((1000, 1000))
+color = (255, 255, 255)
+win.fill(color)
+pygame.display.flip()
+pygame.display.set_caption("First Game")
